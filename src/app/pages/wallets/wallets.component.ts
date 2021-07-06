dd07ee31b6d495ba147ed9499deaf4868bf204a6
robert-upwork <robyop.eyeco@gmail.com>
2021-07-06 09:27:53 +0700

    selecting tab in wallet

--- src/app/pages/wallets/wallets.component.ts
+++ src/app/pages/wallets/wallets.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallets.component.scss']
 })
 export class WalletsComponent implements OnInit {
+  selectedWalletType = 'all';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeWalletType(type) {
+    this.selectedWalletType = type;
   }
 
 }
