7092daf0339141b54df881790a86a14fd0e939f6
robert-upwork <robyop.eyeco@gmail.com>
2021-07-06 09:19:08 +0700

    submenu

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,23 +4,35 @@ import { Router } from '@angular/router';
 
 export interface RouteInfo {
     path: string;
+    subMenu: boolean;
     title: string;
     icon: string;
     activeIcon: string;
     class: string;
+    id?: string;
+    parent?: string;
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard', icon: 'assets/svg/finace.svg', activeIcon: 'assets/svg/finace-active.svg', class: '' },
-    { path: '/wallets', title: 'Wallets', icon: 'assets/svg/wallets.svg', activeIcon: 'assets/svg/wallets-active.svg', class: '' },
-    { path: '/documents', title: 'Documents', icon: 'assets/svg/documents.svg', activeIcon: 'assets/svg/documents-active.svg', class: '' },
-    { path: '/verifications', title: 'Verifications', icon: 'assets/svg/verifications.svg', activeIcon: 'assets/svg/verifications-active.svg', class: '' },
-    { path: '/partners', title: 'Partners', icon: 'assets/svg/partners.svg', activeIcon: 'assets/svg/partners-active.svg', class: '' },
-    { path: '/users', title: 'Users', icon: 'assets/svg/users.svg', activeIcon: 'assets/svg/users-active.svg', class: '' }
+    { path: '/dashboard', title: 'Dashboard', icon: 'assets/svg/finace.svg',
+        activeIcon: 'assets/svg/finace-active.svg', class: '', subMenu: false, id: 'dashboard' },
+    { path: '/wallets', title: 'Wallets', icon: 'assets/svg/wallets.svg',
+        activeIcon: 'assets/svg/wallets-active.svg', class: '', subMenu: false, id: 'wallets' },
+    { path: '/wallets', title: 'Wallet groups', icon: 'assets/svg/wallets.svg',
+        activeIcon: 'assets/svg/wallets-active.svg', class: '', subMenu: true, parent: 'wallets' },
+    { path: '/documents', title: 'Documents', icon: 'assets/svg/documents.svg',
+        activeIcon: 'assets/svg/documents-active.svg', class: '', subMenu: false, id: 'documents' },
+    { path: '/verifications', title: 'Verifications', icon: 'assets/svg/verifications.svg',
+        activeIcon: 'assets/svg/verifications-active.svg', class: '', subMenu: false, id: 'verifications' },
+    { path: '/partners', title: 'Partners', icon: 'assets/svg/partners.svg',
+        activeIcon: 'assets/svg/partners-active.svg', class: '', subMenu: false, id: 'partners' },
+    { path: '/users', title: 'Users', icon: 'assets/svg/users.svg',
+        activeIcon: 'assets/svg/users-active.svg', class: '', subMenu: false, id: 'users' }
 ];
 
 @Component({
     moduleId: module.id,
+    // tslint:disable-next-line: component-selector
     selector: 'sidebar-cmp',
     templateUrl: 'sidebar.component.html',
     styleUrls: ['sidebar.component.scss']
@@ -29,12 +41,16 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
     public menuItems: any[];
     currURL: string;
+    selectedMenuId: any;
     constructor(private router: Router) { this.currURL = this.router.url; }
     ngOnInit() {
         this.menuItems = ROUTES.filter(menuItem => menuItem);
     }
-    urlRouter(path) {
-        this.router.navigate([path]);
+    urlRouter(item) {
+        if (!item.subMenu) {
+            this.selectedMenuId = item.id;
+        }
+        this.router.navigate([item.path]);
         setTimeout(() => {
             this.currURL = this.router.url;
         }, 10);
